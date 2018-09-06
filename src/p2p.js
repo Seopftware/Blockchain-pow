0fcbd902a9cbc2cc8c23a8a754c5a49f087b3e29
Seopftware <inseop0813@gmail.com>
2018-09-06 17:13:30 +0900

    Creating Blockchain P2P Actions

--- src/p2p.js
+++ src/p2p.js
@@ -1,8 +1,40 @@
-const WebSockets = require("ws");
+const WebSockets = require("ws"),
+    BlockChain = require("./blockchain"); // getting the block chain cause there is a data
 
+
+const { getLastBlock } = BlockChain;    
 const sockets = [];
 
 const getSockets = () => sockets;
+
+// Message Types
+const GET_LATEST = "GET_LATEST";
+const GET_ALL = "GET_ALL";
+const BLOCKCHAIN_RESPONSE = "BLOCKCHAIN_RESPONSE";
+
+// Message Creators
+const getLatest = () => {
+    return {
+        type: GET_LATEST,
+        data: null
+    };
+};
+
+const getAll = () => {
+    return {
+        type:GET_ALL,
+        data:null
+    };
+};
+
+const blockchainResponse = (data) => {
+    return {
+        type: BLOCKCHAIN_RESPONSE,
+        data: data
+    };
+};
+
+
 
 
 const startP2PServer = server => {
@@ -17,6 +49,7 @@ const startP2PServer = server => {
 // work when new socket gets connect
 const initSocketConnection = socket => {
     sockets.push(socket);
+    handleSocketError(socket);
     socket.on("message", (data) =>{
         console.log(data);
     });
@@ -25,6 +58,16 @@ const initSocketConnection = socket => {
         socket.send("welcome!")
     }, 5000);
 };
+
+const handleSocketError = ws => {
+    const closeSocketConnection = ws => {
+        ws.close();
+        sockets.splice(sockets.indexOf(ws), 1); // remove death ws from array
+    };
+
+    ws.on("close", () => closeSocketConnection(ws));
+    ws.on("error", () => closeSocketConnection(ws));
+}
 
 // this function take new url where url socket server is running
 const connectToPeers = newPeer => {
