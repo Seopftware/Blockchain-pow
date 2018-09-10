65509b607c65cd2fd09abb7bc6194445329dd19f
Seopftware <inseop0813@gmail.com>
2018-09-10 18:41:33 +0900

    Implementing Proof Work part

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,13 +1,16 @@
-const CryptoJS = require("crypto-js");
+const CryptoJS = require("crypto-js"),
+hexToBinary = require("hex-to-binary")
 
 class Block{
     // the function gets called every time we made new block
-    constructor(index, hash, previousHash, timestamp, data){
+    constructor(index, hash, previousHash, timestamp, data, difficulty, nonce){
         this.index=index;
         this.hash=hash;
         this.previousHash=previousHash;
         this.timestamp=timestamp;
         this.data=data;
+        this.difficulty=difficulty;
+        this.nonce=nonce;
     }
 }
 
@@ -18,7 +21,9 @@ const genesisBlock = new Block(
     "7043EEC2224776975E5B4AA39A2AD575D0F9F70873A232BBF61F627F12601DB0",
     null,
     1535943926.164,
-    "This is the Genesis Block!"
+    "This is the Genesis Block!",
+    0,
+    0
 )
 
 // block chain is array of block
@@ -30,31 +35,67 @@ let blockchain = [genesisBlock];
 const getNewestBlock = () => blockchain[blockchain.length -1];
 const getTimestamp = () => new Date().getTime() / 1000;
 const getBlockchain = () => blockchain;
-const createHash = (index, previousHash, timestamp, data) =>
-    CryptoJS.SHA256(index + previousHash + timestamp + JSON.stringify(data)).toString();
+const createHash = (index, previousHash, timestamp, data, difficulty, nonce) =>
+    CryptoJS.SHA256(index + previousHash + timestamp + JSON.stringify(data + difficulty + nonce)).toString();
 
 const createNewBlock = data => {
    const previousBlock = getNewestBlock();
    const newBlockIndex = previousBlock.index + 1;
    const newTimestamp = getTimestamp();
-   const newHash = createHash(
-       newBlockIndex, 
-       previousBlock.hash, 
-       newTimestamp, 
-       data
-   );
-   const newBlock = new Block(
+
+   const newBlock = findBlock(
        newBlockIndex,
-       newHash,
        previousBlock.hash,
        newTimestamp,
-       data
+       data,
+       10 // dificultty
    );
     addBlockToChain(newBlock); // 새로운 블록 추가
     // p2p는 block chain을 요구, block chain은 p2p를 요구 (circular dependency 발생)
     require("./p2p").broadcastNewBlock(); // circular dependency를 방지하기 위해 임시방편으로.. // import 없이 함수를 부르는 방법
     return newBlock;
 };
+
+const findBlock = (index, previousHash, timestamp, data, difficulty)=>{
+    let nonce = 0;
+
+    // find hash
+    while(true){
+        console.log("Current Nonce", nonce);
+        const hash = createHash(
+            index,
+            previousHash,
+            timestamp,
+            data,
+            difficulty, // difficulty is number of continuous front 0
+            nonce
+        );
+
+        // check amount of zeros (hashMathches Difficulty)
+        if(hashMathchesDifficulty(hash, difficulty)){
+            return new Block(
+                index,
+                hash,
+                previousHash,
+                timestamp,
+                data,
+                difficulty,
+                nonce
+            );
+        }else{
+            nonce++;
+        }
+    }
+}
+
+const hashMathchesDifficulty = (hash, difficulty) => {
+    // hash -> binary
+    const hashInBinary = hexToBinary(hash);
+    const requiredZeros = "0".repeat(difficulty); // difficulty 만큼 0의 갯수가 반복됨.
+    console.log("Trying difficulty:", difficulty, "with hash", hash);
+    // 바이너리의 해쉬 숫자가 난이도 만큼 0을 반복한다면
+    return hashInBinary.startsWith(requiredZeros); // startsWith() => true / false
+}
 
 const getBlockHash = block => createHash(block.index, block.previousHash, block.timestamp, block.data);
 
